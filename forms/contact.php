eea4885312a05a997629728d8d88db3c2ad528fc
Shen8Singh <gayashan9090@gmail.com>
2024-01-08 20:25:48 +0530

    blog fix-1

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,44 @@
+<!-- not complit////////////////////////////////////////////////////////////// 405 err////////// -->
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+require '../assets/vendor/php-email-form/php-email-form.php';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+$receiving_email_address = 'kylix.ijse@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$contact = new PHP_Email_Form();
+$contact->ajax = true;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
+$contact->to = $receiving_email_address;
+$contact->from_name = sanitize_input($_POST['name']);
+$contact->from_email = sanitize_input($_POST['email']);
+$contact->subject = sanitize_input($_POST['subject']);
+
+// Uncomment below code if you want to use SMTP to send emails. Enter your correct SMTP credentials.
+/*
+$contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    'port' => '587',
+    'encryption' => 'tls', // Change to 'ssl' if necessary
+);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+*/
 
-  echo $contact->send();
+$contact->add_message($contact->from_name, 'From');
+$contact->add_message($contact->from_email, 'Email');
+$contact->add_message(sanitize_input($_POST['message']), 'Message', 10);
+
+echo $contact->send();
+
+// Function to sanitize input data
+function sanitize_input($data)
+{
+    return htmlspecialchars(stripslashes(trim($data)));
+}
 ?>
