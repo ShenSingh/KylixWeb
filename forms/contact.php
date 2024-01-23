994893e6ba9e639aeabd98f3f32ee3acfcf38d3b
Shen8Singh <gayashan9090@gmail.com>
2024-01-23 17:13:09 +0530

    Merge remote changes into local branch

--- forms/contact.php
+++ forms/contact.php
@@ -1,44 +1,35 @@
-<!-- not complit////////////////////////////////////////////////////////////// 405 err////////// -->
 <?php
-require '../assets/vendor/php-email-form/php-email-form.php';
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'kylix.ijse@gmail.com';
 
-$receiving_email_address = 'kylix.ijse@gmail.com';
+  // Path to the PHP Email Form library
+  $php_email_form_path = '../assets/vendor/php-email-form/php-email-form.php';
 
-if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
-    include($php_email_form);
-} else {
+  if (file_exists($php_email_form_path)) {
+    include($php_email_form_path);
+  } else {
     die('Unable to load the "PHP Email Form" Library!');
-}
+  }
 
-$contact = new PHP_Email_Form();
-$contact->ajax = true;
+  $contact = new PHP_Email_Form();
+  $contact->ajax = true;
 
-$contact->to = $receiving_email_address;
-$contact->from_name = sanitize_input($_POST['name']);
-$contact->from_email = sanitize_input($_POST['email']);
-$contact->subject = sanitize_input($_POST['subject']);
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
-// Uncomment below code if you want to use SMTP to send emails. Enter your correct SMTP credentials.
-/*
-$contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587',
-    'encryption' => 'tls', // Change to 'ssl' if necessary
-);
+  // SMTP Configuration
+//   $contact->smtp = array(
+//     'host' => 'smtp.your-email-provider.com',
+//     'username' => 'your-smtp-username',
+//     'password' => 'your-smtp-password',
+//     'port' => '587'
+//   );
 
-*/
+  $contact->add_message($_POST['name'], 'From');
+  $contact->add_message($_POST['email'], 'Email');
+  $contact->add_message($_POST['message'], 'Message', 10);
 
-$contact->add_message($contact->from_name, 'From');
-$contact->add_message($contact->from_email, 'Email');
-$contact->add_message(sanitize_input($_POST['message']), 'Message', 10);
-
-echo $contact->send();
-
-// Function to sanitize input data
-function sanitize_input($data)
-{
-    return htmlspecialchars(stripslashes(trim($data)));
-}
+  echo $contact->send();
 ?>
